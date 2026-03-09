f213537cbcb03a009d4b6dae5a12e9731b352cf8
JadenPalo <palominoja3@gmail.com>
2026-03-09 22:43:24 +0000

    ExecutionLog.hpp and ExecutionLog.cpp implementation

--- log/ExecutionLog.cpp
+++ log/ExecutionLog.cpp
@@ -1,1 +1,47 @@
 // implements trade logging
+//Author: Jaden Palomino
+//File: ExecutionLog.cpp
+
+#include "ExecutionLog.hpp"
+#include <iostream>
+#include <iomanip>
+
+void ExecutionLog::recordTrade(const Trade& trade) {
+    trades_.push_back(trade);
+}
+
+void ExecutionLog::recordTrades(const std::vector<Trade>& trades) {
+    for (const auto& t : trades) {
+        trades_.push_back(t);
+    }
+}
+
+int ExecutionLog::size() const {
+    return trades_.size();
+}
+
+void ExecutionLog::display() const {
+    std::cout << "\n=== EXECUTION LOG ===\n";
+
+    if (trades_.empty()) {
+        std::cout << "No Trades Executed Yet.\n";
+        return;
+    }
+    for (const auto& t : trades_) {
+        std::cout << "TRADE: Order #"
+                  << t.buyOrderId
+                  << " matched with Order #"
+                  << t.sellOrderId
+                  << " for "
+                  << t.quantity
+                  << " shares of "
+                  << t.ticker
+                  << " at $"
+                  << std::fixed << std::setprecision(2)
+                  << (t.price / 100.0)
+                  << "\n";
+    }
+    //Sample Print:
+    //TRADE: Order: #101 matched with Order #88 for 50 shares of <xyz> at $102.32
+
+}
--- log/ExecutionLog.hpp
+++ log/ExecutionLog.hpp
@@ -1,1 +1,27 @@
 // stores trade history 
+//Author: Jaden Palomino
+//File: ExecutionLog.cpp
+#pragma once
+#include <vector>
+#include <string>
+#include "Trade.hpp"
+
+class ExecutionLog {
+public:
+    //Add a single trade to History
+    void recordTrade(const Trade& trade);
+    
+    //Add multiple trades to History
+    //Useful for when match() returns several
+    void recordTrades(const std::vector<Trade>& trades);
+
+    //Prints all trades
+    void display() const;
+
+    //Total number of trades executed
+    int size() const;
+
+private:
+    //Stores all executed trades
+    std::vector<Trade> trades_;
+};
